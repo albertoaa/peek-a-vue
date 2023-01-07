004200b99208c91d5efbd4c7b03216ffc2ef91da
Alberto Aragon Alvarez <ronin8503@gmail.com>
2023-01-07 21:42:31 +0000

    initial board layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Peek a Vue</h1>
+  <Card />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/Card.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">Card {{ id }} {{ value }}</div>
+</template>
+
+<script>
+export default {
+  name: "Card-Component",
+  props: {
+    id: Number,
+    value: String,
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
