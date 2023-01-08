a51df8cb662dbce1dff3ebc7dc32a29f5392dc9d
Alberto Aragon Alvarez <ronin8503@gmail.com>
2023-01-08 19:26:45 +0000

    feat: styling board

--- src/App.vue
+++ src/App.vue
@@ -124,13 +124,27 @@ export default {
 </script>
 
 <style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+h1 {
+  margin-top: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-image: url('../public/images/page-bg.png');
+  background-color: #00070c;
+  height: 100vh;
+  color: #fff;
+  padding-top: 60px;
 }
 
 .game-board {
